0c03e17b2554fb7583ec7c5512e9c273dfbe437b
Adam Walter <adam@adamwalter.com>
2014-02-18 01:04:00 -0500

    Add wp-config-local.php

---
+++ wp-config-local.php
@@ -0,0 +1,7 @@
+<?php
+
+/** Local database settings */
+define( 'DB_NAME', 'local_db_name' );
+define( 'DB_USER', 'local_db_user' );
+define( 'DB_PASSWORD', 'local_db_password' );
+define( 'DB_HOST', 'localhost' );
